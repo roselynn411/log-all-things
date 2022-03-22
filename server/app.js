7a7c6211a7f9c662a97d12322a11d79bac8058dc
Roselynn <worklaptop@Roselynns-MacBook-Pro.local>
2022-03-21 23:09:32 -0400

    first commit

--- server/app.js
+++ server/app.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-
-app.use((req, res, next) => {
-// write your logging code here
-
+//require csv to json
+const csvParser = require('csvtojson');
+const csvFilePath = './log.scv';
+let log;
+console.log(log);
+//Write the code required to generate a log line for every request following this format:
+app.use((req, res, next)=> {
+// write your logging code here..
+  //grab userAgent found in request header, reformat to replace commas
+  const reqAgent = req.headers['user-agent'].replace(',', '');
+  //request time; use  ISO date standard
+  //ISOstring is a string representing the given data in ISOS
+  const reqTime = new Date().toISOString();
+  // request method
+  const reqMethod = req.method;
+  //request url
+  const reqResource = req.url;
+  //request http version
+  const reqVersion = `HTTP/${req.httpVersion}`;
+  const reqStatus = 200;
+  log = `${reqAgent},${reqTime},${reqMethod},${reqResource},${reqVersion},${reqStatus}\n`;
+  next(); // after complier goes through 20-23, move into the next middleware
 });
 
-app.get('/', (req, res) => {
-// write your code to respond "ok" here
+//cd go into your root directory
+app.get('/',(req,res) =>{
+  res.status(200).send('ok');
+}
 
-});
 
-app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
 
-});
+//write your code to return a json object containing the log data here
+//get data from csv parser
+
+
+)
 
 module.exports = app;
